6c7e558b1331649f2b7729d030e72fa74a77831a
Le H Ngo <seiji@o2.pl>
2019-05-07 12:04:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // write your code below!
+
+function happyHolidays() {
+  return "Happy Holidays!";
+}
+
+console.log(String(happyHolidays()));
+
+function happyHolidaysTo(name) {
+  return `Happy holidays, ${name}!`;
+}
+
+console.log(happyHolidaysTo("Le Hoang"));
+
+function happyHolidayTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`;
+}
+
+console.log(happyHolidayTo("X-Mas", "Le Hoang"));
+
+function holidayCountDown(days, holiday) {
+  return `It's ${days} days until ${holiday}!`;
+}
+
+console.log(holidayCountDown(30, "X-mas"));
